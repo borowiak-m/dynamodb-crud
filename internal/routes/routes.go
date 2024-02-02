2ba09d118e012ecb16790587c7a28be978d76f60
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-02-02 21:56:04 +0000

    Added func definition in routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,5 +1,13 @@
 package routes
 
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/middleware"
+	//ServerConfig "https://github.com/borowiak-m/dynamodb-crud/config"
+	//HealthHandler "https://github.com/borowiak-m/dynamodb-crud/internal/handlers/health"
+	//ProductHandler "https://github.com/borowiak-m/dynamodb-crud/internal/handlers/product"
+)
+
 type Router struct {
 	config *Config
 	router *chi.Mux
@@ -12,24 +20,73 @@ func NewRouter() *Router {
 	}
 }
 
-func (r *Router) SetRouters() *chi.Mux {
+func (r *Router) SetRouters(repository adapter.Interface) *chi.Mux {
+	r.setConfigRouters()
+	r.RouterHealth(repository)
+	r.RouterProduct(repository)
 
+	return r.router
 }
 
 func (r *Router) setConfigRouters() {
-
+	r.EnableCors()
+	r.EnableLogger()
+	r.EnableTimeout()
+	r.EnableRecovery()
+	r.EnableRequestID()
+	r.EnableRealIP()
 }
 
-func RouterHealth() {}
+func (r *Router) RouterHealth(repository adapter.Interface) {
+	handler := HealthHandler.NewHandler(repository)
 
-func RouterProduct() {}
+	r.router.Route("/health", func(router chi.Router) {
+		router.Post("/", handler.Post)
+		router.Get("/", handler.Get)
+		router.Put("/", handler.Put)
+		router.Delete("/", handler.Delete)
+		router.Options("/", handler.Options)
+	})
+}
 
-func EnableTimeout() {}
+func (r *Router) RouterProduct(repository adapter.Interface) {
+	handler := ProductHandler.NewHandler(repository)
 
-func EnableCors() {}
+	r.router.Route("/product", func(router chi.Router) {
+		router.Post("/", handler.Post)
+		router.Get("/", handler.Get)
+		router.Put("/{ID}", handler.Put)
+		router.Delete("/{ID}", handler.Delete)
+		router.Options("/", handler.Options)
+	})
+}
 
-func EnableRecovery() {}
+func (r *Router) EnableLogger() *Router {
+	r.router.Use(middleware.Logger)
+	return r
+}
 
-func EnableRequestID() {}
+func (r *Router) EnableTimeout() *Router {
+	r.router.Use(middleware.Timeout(r.config.GetTimeout()))
+	return r
+}
 
-func EnableRealIP() {}
+func (r *Router) EnableCors() *Router {
+	r.router.Use(r.config.Cors)
+	return r
+}
+
+func (r *Router) EnableRecovery() *Router {
+	r.router.Use(middleware.Recoverer)
+	return r
+}
+
+func (r *Router) EnableRequestID() *Router {
+	r.router.Use(middleware.RequestID)
+	return r
+}
+
+func (r *Router) EnableRealIP() *Router {
+	r.router.Use(middleware.RealIP)
+	return r
+}
