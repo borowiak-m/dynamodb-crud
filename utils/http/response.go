ece9f80c20113cdb824b9c5e6ba55f9895b026ab
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-02-03 13:00:21 +0000

    Fixed htp.ResponseWriter refs

--- utils/http/response.go
+++ utils/http/response.go
@@ -27,7 +27,7 @@ func (resp *response) string() string {
 	return string(resp.bytes())
 }
 
-func (resp *response) sendResponse(w *http.ResponseWriter, r *http.Request) {
+func (resp *response) sendResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(resp.Status)
@@ -36,41 +36,41 @@ func (resp *response) sendResponse(w *http.ResponseWriter, r *http.Request) {
 }
 
 // 200
-func StatusOK(w *http.ResponseWriter, r *http.Request, data interface{}) {
+func StatusOK(w http.ResponseWriter, r *http.Request, data interface{}) {
 	newResponse(data, http.StatusOK).sendResponse(w, r)
 }
 
 // 204
-func StatusNoContent(w *http.ResponseWriter, r *http.Request) {
+func StatusNoContent(w http.ResponseWriter, r *http.Request) {
 	newResponse(nil, http.StatusNoContent).sendResponse(w, r)
 }
 
 // 400
-func StatusBadRequest(w *http.ResponseWriter, r *http.Request, err error) {
+func StatusBadRequest(w http.ResponseWriter, r *http.Request, err error) {
 	data := map[string]interface{}{"error": err.Error()}
 	newResponse(data, http.StatusBadRequest).sendResponse(w, r)
 }
 
 // 404
-func StatusNotFound(w *http.ResponseWriter, r *http.Request, err error) {
+func StatusNotFound(w http.ResponseWriter, r *http.Request, err error) {
 	data := map[string]interface{}{"error": err.Error()}
 	newResponse(data, http.StatusNotFound).sendResponse(w, r)
 }
 
 // 405
-func StatusMethodNotAllowed(w *http.ResponseWriter, r *http.Request) {
+func StatusMethodNotAllowed(w http.ResponseWriter, r *http.Request) {
 	newResponse(nil, http.StatusMethodNotAllowed).sendResponse(w, r)
 }
 
 // 409
-func StatusConflict(w *http.ResponseWriter, r *http.Request, err error) {
+func StatusConflict(w http.ResponseWriter, r *http.Request, err error) {
 	data := map[string]interface{}{"error": err.Error()}
 	newResponse(data, http.StatusNotFound).sendResponse(w, r)
 }
 
 // 500
 
-func StatusInternalServerError(w *http.ResponseWriter, r *http.Request, err error) {
+func StatusInternalServerError(w http.ResponseWriter, r *http.Request, err error) {
 	data := map[string]interface{}{"error": err.Error()}
 	newResponse(data, http.StatusNotFound).sendResponse(w, r)
 }
