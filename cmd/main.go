d11487c56a6d1aad6506701cc317fd2ed9d5592e
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-02-05 20:08:22 +0000

    Added functions in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,66 @@
 package main
 
-func main() {
+import (
+	"dynamodb-crud/internal/repository/adapter"
+	"dynamodb-crud/internal/repository/instance"
+	"dynamodb-crud/internal/routes"
+	"dynamodb-crud/utils/logger"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	Config "github.com/borowiak-m/dynamodb-crud/config"
+)
+
+func main() {
+	configs := Config.GetConfig()
+	connection := instance.GetConnection()
+	repository := adapter.NewAdapter(connection)
+
+	logger.INFO("waiting for the service to start...", nil)
+
+	errors := Migrate(connection)
+	if len(errors) > 0 {
+		for _, err := range errors {
+			logger.PANIC("Error on migration:.....", err)
+		}
+	}
+
+	logger.PANIC("", checkTables(connection))
+
+	port := fmt.Sprintf(":%v", configs.Port)
+	router := routes.NewRouter().SetRouters(repository)
+	logger.INFO("Service is running on port", port)
+
+	server := http.ListenAndServe(port, router)
+	log.Fatal(server)
 }
 
-func Migrate()
+func Migrate(connection *dynamodb.DynamoDB) []error {
+	var errors []error
+	callMigrateAndAppendError(&errors, connection, &RulesProduct.Rules{})
+	return errors
+}
 
-func checkTables()
+func callMigrateAndAppendError(errors *[]error, connection *dynamodb.DynamoDB, rule rules.Interface) {
+	err := rule.Migrate(connection)
+	if err != nil {
+		*errors = append(*errors, err)
+	}
+}
+
+func checkTables(connection *dynamodb.DynamoDB) error {
+	response, err := connection.ListTables(&dynamodb.ListTables(&dynamodb.ListTablesInput()))
+
+	if response != nil {
+		if len(response.TableNames) == 0 {
+			logger.INFO("Tables not found", nil)
+		}
+
+		for _, tableName := range response.TanleNames {
+			logger.INFO("Table found", *tableName)
+		}
+	}
+	return err
+}
